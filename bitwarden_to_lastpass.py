2c3d063f67bc47544265ab15535cd3bafd48aff8
htlsne <htlsne23@gmail.com>
2018-05-05 13:49:50 +0900

    Add comments

--- bitwarden_to_lastpass.py
+++ bitwarden_to_lastpass.py
@@ -4,7 +4,9 @@ import pandas as pd
 
 df = pd.read_csv('bitwarden.csv')
 
+# Change bitwarden's column names to lastpass's one
 df.rename(columns = {'login_uri': 'url', 'login_username': 'username', 'login_password': 'password', 'notes': 'extra'}, inplace=True)
+# Set url for secure note
 df.loc[df['type'] == 'note', 'url'] = 'http://sn'
 
 df.to_csv('lastpass.csv', index=False)
