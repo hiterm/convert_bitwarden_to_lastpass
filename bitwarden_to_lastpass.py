bdda2c2176f0c8c44fd11a7f080afcdea83a6657
htlsne <htlsne23@gmail.com>
2018-05-05 12:28:27 +0900

    First commit

---
+++ bitwarden_to_lastpass.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+df = pd.read_csv('bitwarden.csv')
+
+df.rename(columns = {'login_uri': 'url', 'login_username': 'username', 'login_password': 'password', 'notes': 'extra'}, inplace=True)
+df.loc[df['type'] == 'note', 'url'] = 'http://sn'
+
+df.to_csv('lastpass.csv')
